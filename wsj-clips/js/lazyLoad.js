8ebb4049ca10d31267e9ec6e7218e635201b2f67
carsonology <61434744+carsonology@users.noreply.github.com>
2021-10-29 18:20:23 -0400

    add js/index files

---
+++ wsj-clips/js/lazyLoad.js
@@ -0,0 +1,34 @@
+'use strict';
+console.log('lazy working');
+
+// lazy load code from https://codepen.io/imagekit_io/pen/MBNwKB
+
+document.addEventListener("DOMContentLoaded", function() {
+    let lazyloadImages = document.querySelectorAll("img.lazy");    
+    let lazyloadThrottleTimeout;
+    
+    function lazyload () {
+      if (lazyloadThrottleTimeout) {
+        clearTimeout(lazyloadThrottleTimeout);
+      }    
+      
+      lazyloadThrottleTimeout = setTimeout(function() {
+          let scrollTop = window.pageYOffset;
+          lazyloadImages.forEach(function(img) {
+              if(img.offsetTop < (window.innerHeight + scrollTop)) {
+                img.src = img.dataset.src;
+                img.classList.remove('lazy');
+              }
+          });
+          if(lazyloadImages.length == 0) { 
+            document.removeEventListener("scroll", lazyload);
+            window.removeEventListener("resize", lazyload);
+            window.removeEventListener("orientationChange", lazyload);
+          }
+      }, 20);
+    }
+    
+    document.addEventListener("scroll", lazyload);
+    window.addEventListener("resize", lazyload);
+    window.addEventListener("orientationChange", lazyload);
+  });
