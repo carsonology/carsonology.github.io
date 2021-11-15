f13f6c67e62e38268b282fc16b07157fa7aa4479
Carson TerBush <61434744+carsonology@users.noreply.github.com>
2021-11-15 15:43:08 -0500

    Add files via upload

---
+++ seattle-clips/js/nameAnim.js
@@ -0,0 +1,19 @@
+'use strict';
+console.log('Javascript working');
+
+// SET UP CSS ANIMATION FOR NAME HEADER
+
+// get full name (desktop) and first + last names (mobile) from DOM
+const name = document.querySelectorAll("#name path");
+const hithere = document.querySelectorAll("#hi-there path");
+
+// set animation delay variable to 0 to start iteration
+let animDelay = .5;
+
+for (let letter of hithere) {
+    let perimeter = letter.getTotalLength(); // get perimeter length of each svg letter
+    letter.style.strokeDasharray = `${perimeter}px`; // set stroke dasharray to the perimeter
+    letter.style.strokeDashoffset = `${perimeter}px`; // set stroke offset to the perimeter
+    letter.style.animation = `line-anim 2s ease forwards ${animDelay}s`; // iterate animation delay for each letter
+    animDelay += 0.1;
+}
