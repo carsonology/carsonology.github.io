b82344ed3060b70b9a3231f1349c5967342decb8
carsonology <carson.terbush@gmail.com>
2023-04-29 17:36:04 -0400

    add animations to nav - still not working

--- src/AnimatedIcon.js
+++ src/AnimatedIcon.js
@@ -1,30 +1,45 @@
 import Lottie from 'react-lottie';
+import { useEffect, useState } from 'react'
 import anim_data_graphics from './animation-data/bar-chart.json'
 import anim_data_design from './animation-data/paint-palette.json'
 import anim_data_writing from './animation-data/notebook.json'
-import anim_data_dev from './animation-data/monitor.json'
+import anim_data_dev from './animation-data/laptop.json'
 import { flatten } from 'lottie-colorify'
+
+import lottieWeb from 'https://cdn.skypack.dev/lottie-web';
 
 
 function AnimatedIcon(props) {
-    const { name, animationStopped } = props
+    const { name, shownSection, hoveredAnimId } = props
+
+    useEffect(() => {
+        // console.log(hoveredAnimId, name, hoveredAnimId !== name)
+        // setStopFlag(hoveredAnimId !== name)
+        console.log(hoveredAnimId)
+        console.log(name, 'stop flag:', hoveredAnimId !== name)
+        // console.log('shown section:', shownSection)
+    }, [hoveredAnimId])
 
     let anim_data = null
 
-    if (name == 'palette') {
+    if (name == 'design') {
         anim_data = anim_data_design
-    } else if (name == 'chart') {
+    } else if (name == 'graphics') {
         anim_data = anim_data_graphics
-    } else if (name == 'laptop') {
+    } else if (name == 'webdev') {
         anim_data = anim_data_dev
-    } else if (name == 'pencil') {
+    } else if (name == 'writing') {
         anim_data = anim_data_writing
     }
+
+    const anim_color = ((name === shownSection) || (name === hoveredAnimId)) ? [104, 104, 104] : [176, 176, 176]
+    // const anim_color = [104, 104, 104]
 
     const defaultOptions = {
         loop: true,
         autoplay: true,
-        animationData: anim_data,
+        animationData: flatten(anim_color, anim_data),
+        // animationData: anim_data,
         rendererSettings: {
             preserveAspectRatio: 'xMidYMid meet'
         }
@@ -35,7 +50,8 @@ function AnimatedIcon(props) {
             options={defaultOptions}
             height={30}
             width={40}
-            isStopped={animationStopped}
+            // if it's not being hovered, it is stopped
+            isStopped={hoveredAnimId === null}
         />
     )
 }
