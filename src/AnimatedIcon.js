cc87770be026c55f98444a8dc5f3c0963c463030
carsonology <carson.terbush@gmail.com>
2023-05-09 12:10:43 -0400

    add menu animation

--- src/AnimatedIcon.js
+++ src/AnimatedIcon.js
@@ -1,24 +1,13 @@
 import Lottie from 'react-lottie';
-import { useEffect, useState } from 'react'
 import anim_data_graphics from './animation-data/bar-chart.json'
 import anim_data_design from './animation-data/paint-palette.json'
 import anim_data_writing from './animation-data/notebook.json'
 import anim_data_dev from './animation-data/laptop.json'
 import { flatten } from 'lottie-colorify'
 
-import lottieWeb from 'https://cdn.skypack.dev/lottie-web';
-
 
 function AnimatedIcon(props) {
     const { name, shownSection, hoveredAnimId } = props
-
-    useEffect(() => {
-        // console.log(hoveredAnimId, name, hoveredAnimId !== name)
-        // setStopFlag(hoveredAnimId !== name)
-        console.log(hoveredAnimId)
-        console.log(name, 'stop flag:', hoveredAnimId !== name)
-        // console.log('shown section:', shownSection)
-    }, [hoveredAnimId])
 
     let anim_data = null
 
@@ -33,13 +22,11 @@ function AnimatedIcon(props) {
     }
 
     const anim_color = ((name === shownSection) || (name === hoveredAnimId)) ? [104, 104, 104] : [176, 176, 176]
-    // const anim_color = [104, 104, 104]
 
     const defaultOptions = {
-        loop: true,
+        loop: false,
         autoplay: true,
         animationData: flatten(anim_color, anim_data),
-        // animationData: anim_data,
         rendererSettings: {
             preserveAspectRatio: 'xMidYMid meet'
         }
