4bc0ab890359410deddc65345c01c0dc7eeeb783
carsonology <carson.terbush@gmail.com>
2023-09-05 23:07:52 -0400

    add select header back

--- src/components/Section.js
+++ src/components/Section.js
@@ -9,14 +9,39 @@ function Section(props) {
             <h2 className="show-mobile-xs">{title}</h2>
 
             <div className={title === "design" ? "grid-2" : ''}>
-                {data.map((clip, i) => {
-                    return (
-                        <Clip
-                            key={i}
-                            {...clip}
-                        />
-                    )
-                })}</div>
+                {title === 'webdev' && (
+                    <>
+                        <h3 style={{ marginTop: 0 }}>💫 Selects</h3>
+                        {data.filter((clip) => clip?.select).map((clip, i) => {
+                            return (
+                                <Clip
+                                    key={i}
+                                    {...clip}
+                                />
+                            )
+                        })}
+                        <h3 style={{ marginTop: "1.5em" }}>🗓️ Recent work</h3>
+                        {data.filter((clip) => !clip?.select).map((clip, i) => {
+                            return (
+                                <Clip
+                                    key={i}
+                                    {...clip}
+                                />
+                            )
+                        })}
+                    </>
+                )}
+                {title !== "webdev" && (
+                    data.map((clip, i) => {
+                        return (
+                            <Clip
+                                key={i}
+                                {...clip}
+                            />
+                        )
+                    })
+                )}
+            </div>
 
         </article>
     )
