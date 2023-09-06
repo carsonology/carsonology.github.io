7887c5602a268cd9f7f6affeed533b97a737b041
carsonology <carson.terbush@gmail.com>
2023-09-05 23:08:04 -0400

    wording change

--- src/components/Header.js
+++ src/components/Header.js
@@ -21,7 +21,7 @@ function Header() {
             <img src="./wave.png" alt="wave emoji" />
 
             <NameAnim />
-            <h1>A designer, developer and graphics reporter.</h1>
+            <h1>Designer, developer & graphics reporter.</h1>
         </header>
     )
 }
