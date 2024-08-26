37fa8e990fea7f5bbb58e6c491da3ab7764643e5
CARSON TERBUSH <CARSON.TERBUSH@GMAIL.COM>
2024-08-25 21:01:21 -0400

    chore: formatting

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,50 +1,36 @@
+import Clip from "./Clip.js"
 
-import Clip from './Clip.js'
+const Section = (props) => {
+  const { title, data } = props
 
-function Section(props) {
-    const { title, data } = props
+  return (
+    <article id={title}>
+      <h2 className="show-mobile-xs">{title}</h2>
 
-    return (
-        <article id={title}>
-            <h2 className="show-mobile-xs">{title}</h2>
-
-            <div className={title === "design" ? "grid-2" : ''}>
-                {title === 'webdev' && (
-                    <>
-                        <h3 style={{ marginTop: 0 }}>💫 Selects</h3>
-                        {data.filter((clip) => clip?.select).map((clip, i) => {
-                            return (
-                                <Clip
-                                    key={i}
-                                    {...clip}
-                                />
-                            )
-                        })}
-                        <h3 style={{ marginTop: "1.5em" }}>🗓️ Recent work</h3>
-                        {data.filter((clip) => !clip?.select).map((clip, i) => {
-                            return (
-                                <Clip
-                                    key={i}
-                                    {...clip}
-                                />
-                            )
-                        })}
-                    </>
-                )}
-                {title !== "webdev" && (
-                    data.map((clip, i) => {
-                        return (
-                            <Clip
-                                key={i}
-                                {...clip}
-                            />
-                        )
-                    })
-                )}
-            </div>
-
-        </article>
-    )
+      <div className={title === "design" ? "grid-2" : ""}>
+        {title === "webdev" && (
+          <>
+            <h3 style={{ marginTop: 0 }}>💫 Selects</h3>
+            {data
+              .filter((clip) => clip?.select)
+              .map((clip, i) => {
+                return <Clip key={i} {...clip} />
+              })}
+            <h3 style={{ marginTop: "1.5em" }}>🗓️ Recent work</h3>
+            {data
+              .filter((clip) => !clip?.select)
+              .map((clip, i) => {
+                return <Clip key={i} {...clip} />
+              })}
+          </>
+        )}
+        {title !== "webdev" &&
+          data.map((clip, i) => {
+            return <Clip key={i} {...clip} />
+          })}
+      </div>
+    </article>
+  )
 }
 
-export default Section;
+export default Section
