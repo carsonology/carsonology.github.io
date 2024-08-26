587225286c4959dddc1dd75855f44c4d935e65d5
CARSON TERBUSH <CARSON.TERBUSH@GMAIL.COM>
2024-08-25 21:32:12 -0400

    feat: show more button

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,34 +1,51 @@
+import { useState } from "react"
 import Clip from "./Clip.js"
 
 const Section = (props) => {
   const { title, data } = props
+
+  const [numShownClips, setNumShownClips] = useState(10)
+
+  const selects = data.filter((clip) => clip?.select)
+  const nonSelects = data.filter((clip) => !clip?.select)
+  const truncate = ["webdev", "graphics"].includes(title)
 
   return (
     <article id={title}>
       <h2 className="show-mobile-xs">{title}</h2>
 
       <div className={title === "design" ? "grid-2" : ""}>
-        {title === "webdev" && (
-          <>
-            <h3 style={{ marginTop: 0 }}>💫 Selects</h3>
-            {data
-              .filter((clip) => clip?.select)
-              .map((clip, i) => {
+        <>
+          {selects.length > 0 && (
+            <>
+              <h3 style={{ marginTop: 0 }}>💫 Selects</h3>
+              {selects.map((clip, i) => {
                 return <Clip key={i} {...clip} />
               })}
-            <h3 style={{ marginTop: "1.5em" }}>🗓️ Recent work</h3>
-            {data
-              .filter((clip) => !clip?.select)
-              .map((clip, i) => {
-                return <Clip key={i} {...clip} />
-              })}
-          </>
-        )}
-        {title !== "webdev" &&
-          data.map((clip, i) => {
-            return <Clip key={i} {...clip} />
-          })}
+              <h3 style={{ marginTop: "1.5em" }}>🗓️ Recent work</h3>
+            </>
+          )}
+          {nonSelects
+            .slice(
+              0,
+              truncate ? numShownClips - selects.length : nonSelects.length
+            )
+            .map((clip, i) => {
+              return <Clip key={i} {...clip} />
+            })}
+        </>
       </div>
+
+      {numShownClips < data.length && (
+        <button
+          className="show-more-button"
+          onClick={() => {
+            setNumShownClips((prev) => prev + 10)
+          }}
+        >
+          Show more
+        </button>
+      )}
     </article>
   )
 }
