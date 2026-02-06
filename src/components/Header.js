4517d78026adb199d0abedb8c0ddf140f66ca73a
carsonology <carson.terbush@gmail.com>
2026-02-05 21:35:55 -0500

    remove twitter button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,31 @@
-import Icon from './Icon.js'
-import NameAnim from './NameAnim.js'
+import Icon from "./Icon.js";
+import NameAnim from "./NameAnim.js";
 
 function SocialButton(props) {
-    const { link, icon } = props
-    return (
-        <a href={link} target="_blank">
-            <Icon name={icon} />
-        </a>
-    )
+  const { link, icon } = props;
+  return (
+    <a href={link} target="_blank">
+      <Icon name={icon} />
+    </a>
+  );
 }
 
 function Header() {
-    return (
-        <header>
-            <div className="socials">
-                <SocialButton link="https://twitter.com/_carsonology" icon="twitter" />
-                <SocialButton link="mailto:carson.terbush@gmail.com" icon="envelope" />
-                <SocialButton link="https://www.linkedin.com/in/carson-terbush-6a1a58193/" icon="linkedin" />
-            </div>
-            <img src="./wave.png" alt="wave emoji" />
+  return (
+    <header>
+      <div className="socials">
+        {/* <SocialButton link="https://twitter.com/_carsonology" icon="twitter" /> */}
+        <SocialButton link="mailto:carson.terbush@gmail.com" icon="envelope" />
+        <SocialButton
+          link="https://www.linkedin.com/in/carson-terbush-6a1a58193/"
+          icon="linkedin"
+        />
+      </div>
+      <img src="./wave.png" alt="wave emoji" />
 
-            <NameAnim />
-            <h1>Designer, developer & graphics reporter.</h1>
-        </header>
-    )
+      <NameAnim />
+      <h1>Designer, developer & graphics reporter.</h1>
+    </header>
+  );
 }
 export default Header;
